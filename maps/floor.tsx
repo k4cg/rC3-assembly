761c4fb14881942fca7e8d02c40bf8bdb9efa0b5
Christian Kellermann <ckeen@pestilenz.org>
2020-12-11 08:54:24 +0100

    add initial tiled project and map

---
+++ maps/floor.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="floor" tilewidth="32" tileheight="32" tilecount="8" columns="8">
+ <image source="floor.png" width="256" height="32"/>
+</tileset>
