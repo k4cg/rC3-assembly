ceeadfb3ae23689cea5fe640ec4c99cd8942b2eb
ckeen <ckeen@pestilenz.org>
2020-12-26 20:59:04 +0100

    add annas tiles

---
+++ anna/annas tiles.tsx
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="Annas Tiles" tilewidth="32" tileheight="32" tilecount="48" columns="8">
+ <image source="annas tiles.png" width="256" height="192"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="300"/>
+   <frame tileid="2" duration="300"/>
+   <frame tileid="4" duration="300"/>
+   <frame tileid="6" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="1">
+  <animation>
+   <frame tileid="1" duration="300"/>
+   <frame tileid="3" duration="300"/>
+   <frame tileid="5" duration="300"/>
+   <frame tileid="7" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="8">
+  <animation>
+   <frame tileid="8" duration="300"/>
+   <frame tileid="10" duration="300"/>
+   <frame tileid="12" duration="300"/>
+   <frame tileid="14" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="9">
+  <animation>
+   <frame tileid="9" duration="300"/>
+   <frame tileid="11" duration="300"/>
+   <frame tileid="13" duration="300"/>
+   <frame tileid="15" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="16">
+  <animation>
+   <frame tileid="16" duration="300"/>
+   <frame tileid="17" duration="300"/>
+   <frame tileid="18" duration="300"/>
+   <frame tileid="19" duration="300"/>
+   <frame tileid="24" duration="300"/>
+   <frame tileid="25" duration="300"/>
+   <frame tileid="26" duration="300"/>
+   <frame tileid="27" duration="300"/>
+   <frame tileid="32" duration="300"/>
+   <frame tileid="33" duration="300"/>
+   <frame tileid="34" duration="300"/>
+   <frame tileid="35" duration="300"/>
+   <frame tileid="40" duration="300"/>
+   <frame tileid="41" duration="300"/>
+   <frame tileid="42" duration="300"/>
+   <frame tileid="43" duration="300"/>
+  </animation>
+ </tile>
+ <tile id="22">
+  <animation>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="23" duration="500"/>
+  </animation>
+ </tile>
+ <tile id="28">
+  <animation>
+   <frame tileid="28" duration="100"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="29" duration="100"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <animation>
+   <frame tileid="30" duration="300"/>
+   <frame tileid="31" duration="300"/>
+   <frame tileid="38" duration="300"/>
+   <frame tileid="39" duration="300"/>
+   <frame tileid="46" duration="300"/>
+   <frame tileid="47" duration="300"/>
+  </animation>
+ </tile>
+</tileset>
