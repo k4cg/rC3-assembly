95878c781af55bc0dde8d10d8d31500ba9562ac4
Christian Carlowitz <cc@cmesh.de>
2021-12-27 23:23:44 +0100

    added cellar

---
+++ tilesets/chrisc/cellar-darkroom.tsx
@@ -0,0 +1,76 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="cellar-darkroom" tilewidth="32" tileheight="32" tilecount="40" columns="5">
+ <image source="cellar-darkroom.png" width="160" height="256"/>
+ <tile id="0">
+  <animation>
+   <frame tileid="0" duration="3000"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="2000"/>
+  </animation>
+ </tile>
+ <tile id="5">
+  <animation>
+   <frame tileid="0" duration="5000"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="1000"/>
+  </animation>
+ </tile>
+ <tile id="10">
+  <animation>
+   <frame tileid="10" duration="2000"/>
+   <frame tileid="11" duration="100"/>
+   <frame tileid="12" duration="500"/>
+   <frame tileid="13" duration="100"/>
+   <frame tileid="14" duration="4000"/>
+  </animation>
+ </tile>
+ <tile id="15">
+  <animation>
+   <frame tileid="0" duration="1000"/>
+   <frame tileid="1" duration="100"/>
+   <frame tileid="2" duration="500"/>
+   <frame tileid="3" duration="100"/>
+   <frame tileid="4" duration="6000"/>
+  </animation>
+ </tile>
+ <tile id="20">
+  <animation>
+   <frame tileid="0" duration="7000"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="4" duration="3000"/>
+  </animation>
+ </tile>
+ <tile id="25">
+  <animation>
+   <frame tileid="0" duration="4000"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="4" duration="4000"/>
+  </animation>
+ </tile>
+ <tile id="30">
+  <animation>
+   <frame tileid="0" duration="6000"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="4" duration="2000"/>
+  </animation>
+ </tile>
+ <tile id="35">
+  <animation>
+   <frame tileid="20" duration="8000"/>
+   <frame tileid="21" duration="100"/>
+   <frame tileid="22" duration="500"/>
+   <frame tileid="23" duration="100"/>
+   <frame tileid="24" duration="1000"/>
+  </animation>
+ </tile>
+</tileset>
